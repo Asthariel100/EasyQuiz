f643dffd9f4976c4a20483b9edcfea35c11e5404
Yohann <yohann.fallas25@gmail.com>
2022-01-02 21:07:42 +0100

    question skip et score v1

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
-function incr(i){
-    i++
-    return i
+var quiz = {
+    index:0,
+    questions:[]
 }
-
-
-async function questionQuizz(){
+async function getquestions(quiz){
     const response = await fetch('db.json')
     const data = await response.json()
-    var a = 0
-    var a = incr(a)
-    document.getElementById('test').textContent = data[a].question
-    document.getElementById('reponse1').textContent = data[a].reponse1[0]
-    document.getElementById('reponse2').textContent = data[a].reponse2[0]
-    document.getElementById('reponse3').textContent = data[a].reponse3[0]
-    document.getElementById('reponse4').textContent = data[a].reponse4[0]
+    quiz.questions=data
 }
-questionQuizz()
+ function questionQuiz(quiz){
+    document.getElementById('test').textContent = quiz.questions[quiz.index].question
+    document.getElementById('reponse1').textContent = quiz.questions[quiz.index].reponse1
+    document.getElementById('reponse2').textContent = quiz.questions[quiz.index].reponse2
+    document.getElementById('reponse3').textContent = quiz.questions[quiz.index].reponse3
+    document.getElementById('reponse4').textContent = quiz.questions[quiz.index].reponse4
+}
+async function startquiz(quiz){
+    await getquestions(quiz)
+    questionQuiz(quiz)
+}
+
+startquiz(quiz)
+//ajout de score sur la bonne reponse//
+function check(){
+    var score = '';
+    var q1 = document.quiz.reponse.value
+    //il faudrais en value l'index//
+    if(q1 == 'reponse1'){
+        score = score + 5
+        document.getElementById('score').textContent = ' Bonne réponse votre score est de ' + score
+    quiz.index=quiz.index +1
+    questionQuiz(quiz)}
+    }
+
